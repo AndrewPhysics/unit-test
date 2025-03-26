f67ad85393850b6a8669b6b1d386514ea26c0d65
ANDRAS SZABO <szabo.andrew@yahoo.com>
2025-03-26 14:41:36 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include "unitTest.h"
 using namespace std;
-int main() {
+int main()
+{
 	unitTest p1;
 	p1.printInput();
 	bool a, b, c, d;
@@ -9,20 +10,18 @@ int main() {
 	int p;
 	for (p = 0; p < 16; p++)
 	{
-		p1.makeInput1(a, b, c, d, p);//the inputs a,b,c,d are generated
-		result[p]=p1.functionToTest1(a, b, c, d);//the inputs are passed to the function which will tested
+		p1.makeInput1(a, b, c, d, p);//inputs a,b,c,d generated
+		result[p] = p1.functionToTest1(a, b, c, d);
 	}
 	for (int i = 0; i < 16; i++)
 	{
-		if (result[i]==true)
+		if (result[i] == true)
 		{
-			cout << "true return at " << i+1 << " -th permutation" << endl;
+			cout << "true return at " << i + 1 << " -th permutation" << endl;
 		}
 		else
 		{
-			cout << "error at " << i+1 << " -th permutation" << endl;
+			cout << "false at " << i + 1 << " -th permutation" << endl;
 		}
 	}
-	system("pause");
-	return 0;
 }
